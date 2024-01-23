28ea1f20a35a0ff6606b45aaf00d88338206aaa7
Hiroki Ito <imainu.1222@gmail.com>
2024-01-24 03:41:10 +0900

    [add]ユーザーに紐付いたボードを作成できるように変更

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,8 +1,22 @@
 class BoardsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
   end
 
   def new
-    @board = Board.new
+    @board = current_user.boards.build
+  end
+
+  def create
+    @board= current_user.boards.build(board_params)
+    if @board.save
+    else
+      render :new
+    end
+  end
+
+  private
+  def board_params
+    params.require(:board).permit(:title, :content)
   end
 end
