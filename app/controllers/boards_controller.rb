adc047a0a9c94a01a6cd3f77c50e2013135c410e
Hiroki Ito <imainu.1222@gmail.com>
2024-02-02 00:07:30 +0900

    [add]flashメッセージの表示を追加

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,7 +10,9 @@ class BoardsController < ApplicationController
   def create
     @board = current_user.boards.build(board_params)
     if @board.save
+      redirect_to board_path(@board), notice: '保存できました'
     else
+      flash.now[:error] = '保存に失敗しました'
       render :new
     end
   end
