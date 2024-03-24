d4e8ce68e449e854fd6b1bf899035e89469d6b46
Hiroki Ito <imainu.1222@gmail.com>
2024-03-25 03:35:58 +0900

    [add]タスク一覧のアイキャッチを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -21,6 +21,8 @@
 #  fk_rails_...  (board_id => boards.id)
 #
 class Task < ApplicationRecord
+  has_one_attached :eyecatch
+
   validates :title, presence:true
   validates :title, length: { minimum: 2, maximum: 100}
   validates :title, format: { with: /\A(?!\@)/ }
